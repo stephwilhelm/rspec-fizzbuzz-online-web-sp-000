64d874f72fdacf85d8ce7b59d2023b574bce0460
sunny-mouse-7530 <wilhelm.steph@gmail.com>
2019-10-24 02:09:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == fizz_3
+    puts "Fizz"
+
+  elsif int % 5 == fizz_5
+    puts "Buzz"
+
+  elsif int % 3 && int % 5 == fizz_15
+    puts "FizzBuzz"
+
+  else int % 4 == nil
+    puts "Nil"
+
+end
